8c919e2f0bde3774cd178c8b6f7db9c1a61cce11
jrwaheed <j.r.waheed@gmail.com>
2021-07-20 21:42:55 +0200

    forgotten file

---
+++ src/testDB_script.sql
@@ -0,0 +1,13 @@
+
+
+create table customer (
+	id int auto_increment primary key,
+	lastName text(50),
+	firstName varchar(50),
+	balance int
+);
+
+insert into customer (lastName, firstName, balance) values ('Coltrane','John', 10000);
+insert into customer (lastName, firstName, balance) values ('Jacksom','Michael', 9454);
+insert into customer (lastName, firstName, balance) values ('Davis','Miles', 10);
+insert into customer (lastName, firstName, balance) values ('Holliday','Billie', 780000);
